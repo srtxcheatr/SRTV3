e1f49ae36833837765726f21b827b8e7678080b4
srtxcheatr <lamatemba087@gmail.com>
2026-07-23 16:57:30 +0545

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -8,8 +8,8 @@ define('DEVELOPER_URL', 'https://srtxcheatr.github.io/srtxcheats/');
 define('BACKEND_URL', 'https://reprov1.onrender.com'); // ← your Node backend URL
 
 define('BANNERS', [
-    ['image' => 'https://i.ibb.co/9HmdjJr1/file-000000008a6481f588e660845aa6efa9.png', 'link' => 'https://your-banner-1-target-link.example.com'],
-    ['image' => 'https://i.ibb.co/fdnhLZhS/1784804121500.png', 'link' => 'https://your-banner-2-target-link.example.com'],
+    ['image' => 'https://i.ibb.co/9HmdjJr1/file-000000008a6481f588e660845aa6efa9.png', 'link' => 'GZOWtlF8OFSeM0E08epGicNHvAh2'],
+    ['image' => 'https://i.ibb.co/fdnhLZhS/1784804121500.png', 'link' => 'GZOWtlF8OFSeM0E08epGicNHvAh2'],
 ]);
 
 
